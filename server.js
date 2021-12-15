4513a57f6b99ea26adca95f32843dfd03343c6c9
PeoplesChamp46 <76560191+PeoplesChamp46@users.noreply.github.com>
2021-12-16 02:41:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/POKECLASH'));
+app.use(express.static(__dirname + '/dist/pokeclash'));
  
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/POKECLASH/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/pokeclash/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
